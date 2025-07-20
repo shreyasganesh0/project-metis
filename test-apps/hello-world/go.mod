4282b2cc98520888e72382c53e658bd52785046e
ArchWayland <vansi.ganeshs@gmail.com>
2025-07-20 17:58:43 -0400

    feat(infra): create dockerfile that runs test hello world app server

---
+++ test-apps/hello-world/go.mod
@@ -0,0 +1,3 @@
+module github.com/shreyasganesh0/project-metis/test-apps/hello-world
+
+go 1.24.3
---
+++ test-apps/hello-world/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "log"
+    "fmt"
+    "net/http"
+)
+
+func main() {
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+        log.Printf("Request reieved from %s\n", r.RemoteAddr);
+        fmt.Fprintf(w, "Hit Hello World!\n")
+    })
+
+    log.Fatal(http.ListenAndServe(":8080", nil));
+}
