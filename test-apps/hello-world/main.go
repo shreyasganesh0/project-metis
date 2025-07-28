20988d1ef544f2db6a01fd35d12412d75764a1f7
ArchWayland <vansi.ganeshs@gmail.com>
2025-07-28 19:26:03 -0400

    feat(hello-world): add http requests counting metric init

--- test-apps/hello-world/main.go
+++ test-apps/hello-world/main.go
@@ -4,15 +4,29 @@ import (
     "log"
     "fmt"
     "net/http"
+   "github.com/prometheus/client_golang/prometheus"
 )
+
+var httpRequestsCounter prometheus.Counter
 
 func main() {
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
+        httpRequestsCounter.Inc()
         log.Printf("Request reieved from %s\n", r.RemoteAddr);
         fmt.Fprintf(w, "Updated Hit Hello World!\n")
     })
 
     log.Fatal(http.ListenAndServe(":8080", nil));
+}
+
+func init() {
+
+    httpRequestsCounter = prometheus.NewCounter(prometheus.CounterOpts{
+        Name: "http-requests-total",
+        Help: "Total number of http requests handled.",
+    });
+
+    prometheus.MustRegister(httpRequestsCounter)
 }
